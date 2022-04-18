fab31ebd5a29a6f9b23c44c59b390edad14ce516
Diego Pinto <diego.pinto357@gmail.com>
2022-04-17 23:27:48 -0300

    Add stuff to bscscan sandbo script

--- src/sandbox/bscScanAPI.js
+++ src/sandbox/bscScanAPI.js
@@ -1,20 +1,47 @@
 import 'dotenv/config';
 import fetch from 'node-fetch';
 
-const bscscanApiKey = process.env.BSCSCAN_API_KEY;
-const polygonApiKey = process.env.POLYGONSCAN_API_KEY;
-const walletAddress = process.env.CRYPTO_WALLET_ADDRESS;
 const titanoContract = '0xBA96731324dE188ebC1eD87ca74544dDEbC07D7f';
 const sphereContract = '0x8d546026012bf75073d8a586f24a5d5ff75b9716';
 
+const network = 'bsc';
+const contract = titanoContract;
+const apikey = process.env[`${network.toUpperCase()}SCAN_API_KEY`];
+
+const tokenbalanceParams = new URLSearchParams({
+  module: 'account',
+  action: 'tokenbalance',
+  contractaddress: contract,
+  address: process.env.CRYPTO_WALLET_ADDRESS,
+  tag: 'latest',
+  apikey,
+});
+
+const txlistParams = new URLSearchParams({
+  module: 'account',
+  action: 'txlist',
+  address: process.env.CRYPTO_WALLET_ADDRESS,
+  startblock: 0,
+  endblock: 99999999,
+  page: 1,
+  offset: 100,
+  sort: 'desc',
+  apikey,
+});
+
+const params = txlistParams;
+
+const url = `https://api.${network}scan.com/api?${params}`;
+
 // const url = `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${titanoContract}&address=${walletAddress}&tag=latest&apikey=${bscscanApiKey}`;
 
-const url = `https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${sphereContract}&address=${walletAddress}&tag=latest&apikey=${polygonApiKey}`;
+// const url = `https://api.polygonscan.com/api?module=account&action=tokenbalance&contractaddress=${sphereContract}&address=${walletAddress}&tag=latest&apikey=${polygonApiKey}`;
 
 (async () => {
   const response = await fetch(url);
-  const { result } = await response.json();
-  const tokenScale = 1e-18;
-  const amount = result * tokenScale;
-  console.log(amount);
+  console.log(await response.json());
+  // const { result } = await response.json();
+  // const tokenScale = 1e-18;
+  // const amount = result * tokenScale;
+  // console.log(amount);
 })();
