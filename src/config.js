274c44c92734356b265f8378fe0e5546516dea10
Diego Pinto <diego.pinto357@gmail.com>
2022-04-21 19:55:07 -0300

    Refactor config file

--- src/config.js
+++ src/config.js
@@ -1,15 +1,9 @@
 export default {
   crypto: {
     networks: {
-      bsc: {
-        host: 'https://api.bscscan.com',
-      },
-      polygon: {
-        host: 'https://api.polygonscan.com',
-      },
-      avalanche: {
-        host: 'https://api.snowtrace.io',
-      },
+      bsc: { host: 'https://api.bscscan.com' },
+      polygon: { host: 'https://api.polygonscan.com' },
+      avalanche: { host: 'https://api.snowtrace.io' },
     },
     tokens: {
       avalanche: {
