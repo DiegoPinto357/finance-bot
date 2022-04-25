29088caec3c689de99ba4cfdb080681bf68fc82c
Diego Pinto <diego.pinto357@gmail.com>
2022-04-25 19:33:47 -0300

    Prevent logger to run on test env

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -1,7 +1,10 @@
 export const buildLogger = module => message => {
+  if (process.env.NODE_ENV === 'test') return;
+
   const date = new Date();
   const timestamp = date.toLocaleString('sv', {
     timeZone: 'America/Sao_Paulo',
   });
+
   console.log(`${timestamp}: [${module}] ${message}`);
 };
