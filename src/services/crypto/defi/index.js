8c81f4c057130232cb463e09e5bcca891e453001
Diego Pinto <diego.pinto357@gmail.com>
2022-05-15 00:04:56 -0300

    Remove unnecessary data from defi float

--- src/services/crypto/defi/index.js
+++ src/services/crypto/defi/index.js
@@ -105,31 +105,29 @@ const getLiquidityPoolBalance = async () => {
 const getFloatBalance = async () => {
   const balance = await googleSheets.loadSheet('crypto-defi-float');
   return await Promise.all(
-    balance.map(
-      async ({ asset, depositBRL, depositAmount, network, amount }) => {
-        const priceBRL = await coinMarketCap.getSymbolPrice(asset, network);
+    balance.map(async ({ asset, network }) => {
+      const priceBRL = await coinMarketCap.getSymbolPrice(asset, network);
 
-        const currentAmount =
-          amount === undefined
-            ? await blockchain.getTokenBalance({ asset, network })
-            : amount;
-        const positionBRL = currentAmount * priceBRL;
+      const currentAmount = await blockchain.getTokenBalance({
+        asset,
+        network,
+      });
+      const positionBRL = currentAmount * priceBRL;
 
-        return {
-          type: 'float',
-          asset,
-          description: `${asset} token`,
-          depositBRL,
-          depositAmount,
-          currentAmount,
-          sellFee: undefined,
-          performanceFee: undefined,
-          endDate: undefined,
-          priceBRL,
-          positionBRL,
-        };
-      }
-    )
+      return {
+        type: 'float',
+        asset,
+        description: `${asset} token`,
+        depositBRL: 0,
+        depositAmount: 0,
+        currentAmount,
+        sellFee: undefined,
+        performanceFee: undefined,
+        endDate: undefined,
+        priceBRL,
+        positionBRL,
+      };
+    })
   );
 };
 
