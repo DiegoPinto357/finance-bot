49ad7986aacdca8b11b96b32e526876f44a2e627
Diego Pinto <diego.pinto357@gmail.com>
2022-05-21 03:36:47 -0300

    Enhance HTTP server

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
+#!/usr/bin/env node
+import 'dotenv/config';
+import 'express-async-errors';
 import express from 'express';
+import { buildLogger } from './libs/logger';
+import cryptoRouter from './modules/crypto/crypto.router';
 
 const app = express();
-
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('hello world');
+const log = buildLogger('HTTP Server');
+
+app.all('*', (req, _res, next) => {
+  log(`Request at "${req.originalUrl}"`);
+  next();
 });
 
-app.listen(port, () =>
-  console.log(`Finance Bot - HTTP server listening on port ${port}`)
-);
+app.use(cryptoRouter);
+
+app.use((err, _req, res, _next) => {
+  log(err.stack);
+  res.status(500).json({ error: err });
+});
+
+app.listen(port, () => log(`Finance Bot listening on port ${port}`));
