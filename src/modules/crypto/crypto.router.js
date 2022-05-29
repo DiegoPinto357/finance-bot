9be93029346e5c3d1d310450b813ae13759dced6
Diego Pinto <diego.pinto357@gmail.com>
2022-05-29 01:53:28 -0300

    Add /api prefix to crypto routes

--- src/modules/crypto/crypto.router.js
+++ src/modules/crypto/crypto.router.js
@@ -3,15 +3,15 @@ import cryptoService from './crypto.service';
 
 const router = express.Router();
 
-router.get('/crypto/balance/:type', async (req, res) =>
+router.get('/api/crypto/balance/:type', async (req, res) =>
   res.json(await cryptoService.getBalance(req.params.type))
 );
 
-router.get('/crypto/total/:type', async (req, res) =>
+router.get('/api/crypto/total/:type', async (req, res) =>
   res.json(await cryptoService.getTotalPosition(req.params.type))
 );
 
-router.get('/crypto/history/:type', async (req, res) =>
+router.get('/api/crypto/history/:type', async (req, res) =>
   res.json(await cryptoService.getHistory(req.params.type))
 );
 
