227210ab7a8a86c0d4e84e29c8d9a08ca0877efd
Diego Pinto <diego.pinto357@gmail.com>
2022-09-16 18:05:01 -0300

    Add JSON5 support

--- src/libs/storage.js
+++ src/libs/storage.js
@@ -1,7 +1,8 @@
 import { promises as fs } from 'fs';
+import JSON5 from 'json5';
 
 export const loadFile = async filename =>
-  JSON.parse(await fs.readFile(filename, 'utf-8'));
+  JSON5.parse(await fs.readFile(filename, 'utf-8'));
 
 export const saveFile = (filename, data) =>
-  fs.writeFile(filename, JSON.stringify(data, null, 2), 'utf-8');
+  fs.writeFile(filename, JSON5.stringify(data, null, 2), 'utf-8');
