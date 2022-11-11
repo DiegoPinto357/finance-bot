a28cb10953a5056995b6095cda731301fd8039d3
Diego Pinto <diego.pinto357@gmail.com>
2022-11-11 00:47:25 -0300

    Add options to log function

--- src/libs/logger.js
+++ src/libs/logger.js
@@ -1,10 +1,21 @@
-export const buildLogger = module => message => {
-  if (process.env.NODE_ENV === 'test') return;
+const separatorLength = 100;
 
-  const date = new Date();
-  const timestamp = date.toLocaleString('sv', {
-    timeZone: 'America/Sao_Paulo',
-  });
+export const buildLogger =
+  module =>
+  (
+    message,
+    { breakLineAbove, breakLineBelow, separatoAbove, separatorBelow } = {}
+  ) => {
+    if (process.env.NODE_ENV === 'test') return;
 
-  console.log(`${timestamp}: [${module}] ${message}`);
-};
+    const date = new Date();
+    const timestamp = date.toLocaleString('sv', {
+      timeZone: 'America/Sao_Paulo',
+    });
+
+    if (breakLineAbove) console.log();
+    if (separatoAbove) console.log(separatoAbove.repeat(separatorLength));
+    console.log(`${timestamp}: [${module}] ${message}`);
+    if (separatorBelow) console.log(separatorBelow.repeat(separatorLength));
+    if (breakLineBelow) console.log();
+  };
