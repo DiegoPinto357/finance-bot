83de855bac70a3ac597d29a12d186b4db3caa529
Diego Pinto <diego.pinto357@gmail.com>
2022-11-22 00:39:16 -0300

    Refactor processScriptFile

--- src/modules/processScript/processScriptFile.js
+++ src/modules/processScript/processScriptFile.js
@@ -5,49 +5,46 @@ import processScript from './processScript.service';
 
 const getFileExtension = filename => filename.split('.').pop().toLowerCase();
 
-export default async filename => {
-  const fileExtension = getFileExtension(filename);
+const loadJson5Script = async filename => {
+  const rawFile = await fs.readFile(filename, 'utf-8');
+  const script = await fleece.evaluate(rawFile);
 
-  let rawFile;
-  let script;
+  const disabledScript = fleece.patch(rawFile, { ...script, enable: false });
+  await fs.writeFile(filename, disabledScript, 'utf-8');
 
-  if (fileExtension === 'json5') {
-    rawFile = await fs.readFile(filename, 'utf-8');
-    script = await fleece.evaluate(rawFile);
-  }
+  return script;
+};
+
+const loadJsScript = async filename => {
+  const rawFileBuffer = await fs.readFile(filename, 'utf-8');
+  const rawFile = rawFileBuffer.toString();
 
   const enableFieldMetadataFinderExp =
     /(\/\*\*\n\s\*\s@enable\s)(.*)(\n\s\*\/)/gm;
+  const regexResult = enableFieldMetadataFinderExp.exec(rawFile);
+  const enable = regexResult && regexResult[2];
 
-  if (fileExtension === 'js') {
-    const rawFileBuffer = await fs.readFile(filename, 'utf-8');
-    rawFile = rawFileBuffer.toString();
+  const module = await import(path.join('../../../', filename));
+  const script = module.default;
+  script.enable = enable === 'true';
 
-    const regexResult = enableFieldMetadataFinderExp.exec(rawFile);
-    const enable = regexResult && regexResult[2];
+  const disabledScript = rawFile.replace(
+    enableFieldMetadataFinderExp,
+    '$1false$3'
+  );
+  await fs.writeFile(filename, disabledScript, 'utf-8');
 
-    const module = await import(path.join('../../../', filename));
-    script = module.default;
-    script.enable = enable === 'true';
-  }
+  return script;
+};
 
+export default async filename => {
+  const fileExtension = getFileExtension(filename);
+
+  const script =
+    fileExtension === 'json5'
+      ? await loadJson5Script(filename)
+      : await loadJsScript(filename);
   const results = await processScript(script);
-
-  if (results.status === 'ok') {
-    if (fileExtension === 'json5') {
-      script.enable = false;
-      const disabledScript = fleece.patch(rawFile, script);
-      await fs.writeFile(filename, disabledScript, 'utf-8');
-    }
-
-    if (fileExtension === 'js') {
-      const disabledScript = rawFile.replace(
-        enableFieldMetadataFinderExp,
-        '$1false$3'
-      );
-      await fs.writeFile(filename, disabledScript, 'utf-8');
-    }
-  }
 
   console.dir(results, { depth: null });
 };
