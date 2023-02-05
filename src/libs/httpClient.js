39eb602c92194bc92ac24ef579d0e0b33a36655b
Diego Pinto <diego.pinto357@gmail.com>
2023-02-05 15:10:16 -0300

    Add 5000ms timeout to axios

--- src/libs/httpClient.js
+++ src/libs/httpClient.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const httpClient = axios.create({
+  timeout: 5000,
+});
+
 const get = async url => {
-  const response = await axios(url);
+  const response = await httpClient(url);
   return response.data;
 };
 
