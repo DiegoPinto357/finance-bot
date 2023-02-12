ae2dced3bdf7482e39906c7d57cff3c94b32a537
Diego Pinto <diego.pinto357@gmail.com>
2023-02-12 01:31:23 -0300

    Refactor fs mock

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -4,15 +4,19 @@ export const mockFile = (filename, data) => (files[filename] = data);
 
 export const clearMockFiles = () => (files = {});
 
+const readFile = jest.fn(filename => {
+  const data = files[filename];
+  if (!data) {
+    throw new Error(`ENOENT: no such file or directory, open '${filename}'`);
+  }
+  return files[filename];
+});
+
+const writeFile = jest.fn((filename, data) =>
+  Promise.resolve((files[filename] = data))
+);
+
 export const promises = {
-  readFile: jest.fn(filename => {
-    const data = files[filename];
-    if (!data) {
-      throw new Error(`ENOENT: no such file or directory, open '${filename}'`);
-    }
-    return files[filename];
-  }),
-  writeFile: jest.fn((filename, data) =>
-    Promise.resolve((files[filename] = data))
-  ),
+  readFile,
+  writeFile,
 };
