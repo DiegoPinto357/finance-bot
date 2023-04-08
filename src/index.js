da55b95729d209c23128019df6674e69be77b7fd
Diego Pinto <diego.pinto357@gmail.com>
2023-04-08 00:26:57 -0300

    Setup package config

---
+++ src/index.js
@@ -0,0 +1,4 @@
+export { fixed } from './modules/fixed/fixed.service';
+export { stock } from './modules/stock/stock.service';
+export { crypto } from './modules/crypto/crypto.service';
+export { portfolio } from './modules/portfolio/portfolio.service';
