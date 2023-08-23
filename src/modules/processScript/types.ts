9200ca3113f928e453e6dc4b15233090b70863f0
Diego Pinto <diego.pinto357@gmail.com>
2023-08-23 14:49:59 -0300

    Convert liquidity script to typescript

--- src/modules/processScript/types.ts
+++ src/modules/processScript/types.ts
@@ -11,6 +11,8 @@ type StockMethod = keyof typeof stockService;
 type CryptoMethod = keyof typeof cryptoService;
 export type Method = PortfolioMethod | FixedMethod | StockMethod | CryptoMethod;
 
+type FixedAction = FixedActions.SetAssetValue;
+
 type PortfolioAction =
   | PortfolioActions.Deposit
   | PortfolioActions.Transfer
@@ -19,11 +21,26 @@ type PortfolioAction =
   | PortfolioActions.MoveToPortfolio
   | PortfolioActions.UpdateTables;
 
-type Action = PortfolioAction;
+type Action = FixedAction | PortfolioAction;
 
 export interface Script {
   enable: boolean;
   actions: Action[];
+}
+
+export namespace FixedActions {
+  type SetAssetValueParams = Parameters<typeof fixedService.setAssetValue>[0];
+
+  interface Base {
+    skip?: boolean;
+    module: 'fixed';
+  }
+
+  export interface SetAssetValue extends Base {
+    method: 'setAssetValue';
+    defaultParams?: Partial<SetAssetValueParams>;
+    params?: Partial<SetAssetValueParams> | Partial<SetAssetValueParams>[];
+  }
 }
 
 export namespace PortfolioActions {
@@ -35,49 +52,42 @@ export namespace PortfolioActions {
     typeof portfolioService.moveToPortfolio
   >[0];
 
-  export interface Deposit {
+  interface Base {
     skip?: boolean;
     module: 'portfolio';
+  }
+
+  export interface Deposit extends Base {
     method: 'deposit';
     defaultParams?: Partial<DepositParams>;
     params?: Partial<DepositParams> | Partial<DepositParams>[];
   }
 
-  export interface Transfer {
-    skip?: boolean;
-    module: 'portfolio';
+  export interface Transfer extends Base {
     method: 'transfer';
     defaultParams?: Partial<TransferParams>;
     params?: Partial<TransferParams> | Partial<TransferParams>[];
   }
 
-  export interface Swap {
-    skip?: boolean;
-    module: 'portfolio';
+  export interface Swap extends Base {
     method: 'swap';
     defaultParams?: Partial<SwapParams>;
     params?: Partial<SwapParams> | Partial<SwapParams>[];
   }
 
-  export interface Distribute {
-    skip?: boolean;
-    module: 'portfolio';
+  export interface Distribute extends Base {
     method: 'distribute';
     defaultParams?: Partial<DistributeParams>;
     params?: Partial<DistributeParams> | Partial<DistributeParams>[];
   }
 
-  export interface MoveToPortfolio {
-    skip?: boolean;
-    module: 'portfolio';
+  export interface MoveToPortfolio extends Base {
     method: 'moveToPortfolio';
     defaultParams?: Partial<MoveToPortfolioParams>;
     params?: Partial<MoveToPortfolioParams> | Partial<MoveToPortfolioParams>[];
   }
 
-  export interface UpdateTables {
-    skip?: boolean;
-    module: 'portfolio';
+  export interface UpdateTables extends Base {
     method: 'updateTables';
     // TODO check if this is ok
     defaultParams?: never;
