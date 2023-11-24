ae95acb8240ca1c4f6cbf5a52684eb3412562ad6
Diego Pinto <diego.pinto357@gmail.com>
2023-11-24 19:55:30 -0300

    Add TODO comment

--- src/libs/cache.ts
+++ src/libs/cache.ts
@@ -44,43 +44,45 @@ interface Options {
 }
 
 export const withCache =
-  (func: (...params: any) => any, options: Options = {}) =>
-  async (...params: any) => {
-    if (config.cache.disabled) {
-      return await func(...params);
-    }
+  // TODO use generic type
 
-    const key = hash({ func, params });
-    const cacheEntry = cache[key];
-    const { dataNode } = options;
-
-    if (cacheEntry) {
-      const now = Date.now();
-      const { timestamp } = cacheEntry;
-      const timeToLive = options.timeToLive || config.cache.defaultTimeToLive;
-
-      const expired = Boolean(timeToLive) && timestamp + timeToLive < now;
-      if (!expired) {
-        return dataNode ? { [dataNode]: cacheEntry.data } : cacheEntry.data;
+    (func: (...params: any) => any, options: Options = {}) =>
+    async (...params: any) => {
+      if (config.cache.disabled) {
+        return await func(...params);
       }
-    }
 
-    try {
-      const result = await func(...params);
-      const data = dataNode ? result[dataNode] : result;
-      cache[key] = { data, timestamp: Date.now() };
-      return result;
-    } catch (error) {
+      const key = hash({ func, params });
+      const cacheEntry = cache[key];
+      const { dataNode } = options;
+
       if (cacheEntry) {
-        // TODO identify function
-        log(`Error calling function. Loading stale cache.`, {
-          severity: 'warn',
-        });
-        return dataNode ? { [dataNode]: cacheEntry.data } : cacheEntry.data;
+        const now = Date.now();
+        const { timestamp } = cacheEntry;
+        const timeToLive = options.timeToLive || config.cache.defaultTimeToLive;
+
+        const expired = Boolean(timeToLive) && timestamp + timeToLive < now;
+        if (!expired) {
+          return dataNode ? { [dataNode]: cacheEntry.data } : cacheEntry.data;
+        }
       }
-      throw error;
-    }
-  };
+
+      try {
+        const result = await func(...params);
+        const data = dataNode ? result[dataNode] : result;
+        cache[key] = { data, timestamp: Date.now() };
+        return result;
+      } catch (error) {
+        if (cacheEntry) {
+          // TODO identify function
+          log(`Error calling function. Loading stale cache.`, {
+            severity: 'warn',
+          });
+          return dataNode ? { [dataNode]: cacheEntry.data } : cacheEntry.data;
+        }
+        throw error;
+      }
+    };
 
 export default {
   init,
