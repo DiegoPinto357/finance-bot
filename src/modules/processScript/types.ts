0e06ccc10ac303dc70f56f84f93411ffced01c0b
Diego Pinto <diego.pinto357@gmail.com>
2023-12-22 13:41:27 -0300

    Refactor script types

--- src/modules/processScript/types.ts
+++ src/modules/processScript/types.ts
@@ -13,6 +13,10 @@ export type Method = PortfolioMethod | FixedMethod | StockMethod | CryptoMethod;
 
 type FixedAction = FixedActions.SetAssetValue;
 
+type StockAction = StockActions.Buy | StockActions.SetAssetValue;
+
+type CryptoAction = CryptoActions.Sell;
+
 type PortfolioAction =
   | PortfolioActions.Deposit
   | PortfolioActions.Transfer
@@ -21,56 +25,63 @@ type PortfolioAction =
   | PortfolioActions.MoveToPortfolio
   | PortfolioActions.UpdateTables;
 
-type Action = FixedAction | PortfolioAction;
+type Action = FixedAction | StockAction | CryptoAction | PortfolioAction;
 
-export interface Script {
+export type Script = {
   enable: boolean;
   actions: Action[];
-}
+};
 
 export namespace FixedActions {
   type SetAssetValueParams = Parameters<typeof fixedService.setAssetValue>[0];
 
-  interface Base {
+  type Base = {
     skip?: boolean;
     module: 'fixed';
-  }
+  };
 
-  export interface SetAssetValue extends Base {
+  export type SetAssetValue = Base & {
     method: 'setAssetValue';
     defaultParams?: Partial<SetAssetValueParams>;
     params?: Partial<SetAssetValueParams> | Partial<SetAssetValueParams>[];
-  }
+  };
 }
 
 export namespace StockActions {
   type BuyParams = Parameters<typeof stockService.buy>[0];
+  type SetAssetValueParams = Parameters<typeof stockService.setAssetValue>[0];
 
-  interface Base {
+  type Base = {
     skip?: boolean;
     module: 'stock';
-  }
+  };
 
-  export interface Buy extends Base {
+  export type Buy = Base & {
     method: 'buy';
     defaultParams?: Partial<BuyParams>;
     params?: Partial<BuyParams> | Partial<BuyParams>[];
-  }
+  };
+
+  export type SetAssetValue = Base & {
+    method: 'setAssetValue';
+    defaultParams?: Partial<SetAssetValueParams>;
+    params?: Partial<SetAssetValueParams> | Partial<SetAssetValueParams>[];
+  };
 }
 
 export namespace CryptoActions {
   type SellParams = Parameters<typeof cryptoService.sell>[0];
 
-  interface Base {
+  type Base = {
     skip?: boolean;
     module: 'crypto';
-  }
+  };
 
-  export interface Sell extends Base {
+  export type Sell = Base & {
     method: 'sell';
     defaultParams?: Partial<SellParams>;
     params?: Partial<SellParams> | Partial<SellParams>[];
-  }
+  };
 }
 
 export namespace PortfolioActions {
@@ -82,45 +93,45 @@ export namespace PortfolioActions {
     typeof portfolioService.moveToPortfolio
   >[0];
 
-  interface Base {
+  type Base = {
     skip?: boolean;
     module: 'portfolio';
-  }
+  };
 
-  export interface Deposit extends Base {
+  export type Deposit = Base & {
     method: 'deposit';
     defaultParams?: Partial<DepositParams>;
     params?: Partial<DepositParams> | Partial<DepositParams>[];
-  }
+  };
 
-  export interface Transfer extends Base {
+  export type Transfer = Base & {
     method: 'transfer';
     defaultParams?: Partial<TransferParams>;
     params?: Partial<TransferParams> | Partial<TransferParams>[];
-  }
+  };
 
-  export interface Swap extends Base {
+  export type Swap = Base & {
     method: 'swap';
     defaultParams?: Partial<SwapParams>;
     params?: Partial<SwapParams> | Partial<SwapParams>[];
-  }
+  };
 
-  export interface Distribute extends Base {
+  export type Distribute = Base & {
     method: 'distribute';
     defaultParams?: Partial<DistributeParams>;
     params?: Partial<DistributeParams> | Partial<DistributeParams>[];
-  }
+  };
 
-  export interface MoveToPortfolio extends Base {
+  export type MoveToPortfolio = Base & {
     method: 'moveToPortfolio';
     defaultParams?: Partial<MoveToPortfolioParams>;
     params?: Partial<MoveToPortfolioParams> | Partial<MoveToPortfolioParams>[];
-  }
+  };
 
-  export interface UpdateTables extends Base {
+  export type UpdateTables = Base & {
     method: 'updateTables';
     // TODO check if this is ok
     defaultParams?: never;
     params?: never;
-  }
+  };
 }
