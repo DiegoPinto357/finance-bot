6b4a16820881903c52d829ffd4e5159c7f74928a
Diego Pinto <diego.pinto357@gmail.com>
2024-01-26 18:21:44 -0300

    Add error handling to auto launcher

--- src/autoLauncher.ts
+++ src/autoLauncher.ts
@@ -8,10 +8,24 @@ const log = buildLogger('Auto Launcher');
 (async () => {
   log('Auto Launcher started');
 
-  // await notionDashboard.log(
-  //   `${new Date().toISOString()} - [Module name]: Message`
-  // );
+  try {
+    await notionDashboard.log(
+      `${new Date().toISOString()} - [Auto Launcher]: Updating crypto HODL table`
+    );
 
-  const { balance } = await cryptoService.getBalance('hodl');
-  await notionDashboard.updateCryptoHodlTable(balance);
+    const { balance } = await cryptoService.getBalance('hodl');
+    await notionDashboard.updateCryptoHodlTable(balance);
+
+    process.exit(0);
+  } catch (error) {
+    let errorMessage;
+    if (error instanceof Error) errorMessage = error.message;
+    else errorMessage = String(error);
+
+    await notionDashboard.log(
+      `${new Date().toISOString()} - [Auto Launcher]: ${errorMessage}`
+    );
+    log(errorMessage);
+    process.exit(1);
+  }
 })();
