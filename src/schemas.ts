cde24db2149cbba627d356f4ab704cb21b708f67
Diego Pinto <diego.pinto357@gmail.com>
2025-04-09 14:32:23 -0300

    Update scripts

--- src/schemas.ts
+++ src/schemas.ts
@@ -11,7 +11,7 @@ export const positiveCurrencySchema = z
 const FIXED_ASSET = [
   'nubank',
   'nubankCashback',
-  'iti',
+  'iti', // deprecated
   'inco',
   'xpWesternAsset',
   'xpTrendInvestback',
